2c1b2bdfc821e456cf6e78f8d6fa2a575cc6394b
flavien <f.marianacci@gmail.com>
2017-10-04 17:42:04 +0200

    add test scripts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', script: "javascripts/my_scripts.js" });
 });
+
+router.get('/otherr', function(req, res, next) {
+  res.render('index', { title: 'Other' });
+});
+
 
 module.exports = router;
